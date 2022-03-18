5e7817fd26def53d7dfd6fe37f16dfd570e6c750
Ben Gondzur <dzurgon@gmail.com>
2022-03-18 14:18:45 -0500

    implement intake startstop raiselower & periodic

--- Team 931 G20/src/main/cpp/subsystems/Intake.cpp
+++ Team 931 G20/src/main/cpp/subsystems/Intake.cpp
@@ -2,19 +2,30 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "subsystems/Intake.h"
+# include "subsystems/Intake.h"
 # include "Constants.h"
 using namespace Constants::Intake;
 
 Intake::Intake() : wheels (whnum, rev::CANSparkMax::MotorType::kBrushless),
- actuator(frc::PneumaticsModuleType::CTREPCM, actnum),
+ raiser(frc::PneumaticsModuleType::CTREPCM, actnum),
  running (false) {
   // Implementation of subsystem constructor goes here.
 }
 
 void Intake::Periodic() {
   // Implementation of subsystem periodic method goes here.
+  if (running) wheels.Set(1);
+  else wheels.Set(0);
 }
+
+void Intake::startstop(bool on) {
+  running = on;
+}
+
+void Intake::raiselower(bool on) {
+  raiser.Set(on);
+}
+
 
 void Intake::SimulationPeriodic() {
   // Implementation of subsystem simulation periodic method goes here.
