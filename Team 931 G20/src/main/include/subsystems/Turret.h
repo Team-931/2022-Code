313a9f3214aaa8ab17955f2c68112883e8256490
Nevin Ndonwi <nevinfonndonwi@gmail.com>
2022-03-19 15:32:52 -0500

    Created Turret Header File

---
+++ Team 931 G20/src/main/include/subsystems/Turret.h
@@ -0,0 +1,37 @@
+//This will store functionalities for the turret on the bot
+# include <rev/cansparkmax.h>
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc/encoder.h>
+
+class Turret : public frc2::SubsystemBase {
+ public:
+  Turret();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs during
+   * simulation.
+   */
+
+  void ShootTheBall(); //Shoots the ball into the basket
+
+  void ModifyAngle(); //Modifies the angle at which the turret is set (up down/ how much i is it angled)
+
+  void RotateTurret(); //rotates the turret (has a set amount of degrees at which it can be turned)
+
+     
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+
+   rev::CANSparkMax rotator; // motor used to rotate the turret
+   rev::CANSparkMax anglechanger; //used to modify the angle at which the turret is at (up/down in degrees)
+   frc::Encoder rotatorsensor; //senses the rotation of the turret (how much it has moved) (encoder underneath the cannon-hole) 
+
+};
